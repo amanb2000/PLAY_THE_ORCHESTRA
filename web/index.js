989441f15d149832c6dd7dfe5c1d0d1760710bff
Adam Carnaffan <acarnaffan@hotmail.com>
2019-02-16 21:52:58 -0500

    Aded Sprites

--- web/index.js
+++ web/index.js
@@ -1,8 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var path = require('path');
 
 var connCount = 0
+var sprites = ['staff.png', 'quarter.png', 'sharp.png', 'quarterP.png', 'ledger_line.png'];
 
 app.get('/', function(req, res){
   if (connCount < 1) {
@@ -15,6 +18,27 @@ app.get('/', function(req, res){
 io.on('connection', function(socket) {
   console.log('Connection Made');
   connCount++;
+  sprites.forEach(function(elem) {
+    socket.emit("image-notify");
+    var readStream = fs.createReadStream(path.resolve(__dirname, './sprites/' + elem), {
+      encoding: 'binary'
+    }), chunks = [];
+
+    readStream.on('readable', function() {
+      console.log("Loading img");
+    });
+
+    readStream.on('data', function(chunk) {
+      console.log("ASda");
+      chunks.push(chunk);
+      socket.emit('serve-image-chunk', chunk);
+    });
+
+    readStream.on('end', function() {
+      console.log("Image loaded");
+    });
+  });
+
   socket.on('disconnection', function(socket) {
     connCount--;
     console.log("Disconnected");
